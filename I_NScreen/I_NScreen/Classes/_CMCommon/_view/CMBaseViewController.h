dfb3fce4a3cb17b3df426ad7d5a6ec405b9abde8
bjm <bjm.co.jp@naver.com>
2015-09-12 14:57:08 +0900

    폴더 생성

---
+++ I_NScreen/I_NScreen/Classes/_CMCommon/_view/CMBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMBaseViewController.h
+//  I_NScreen
+//
+//  Created by 조백근 on 2015. 9. 12..
+//  Copyright © 2015년 STVN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMBaseViewController : UIViewController
+
+@end
---
+++ I_NScreen/I_NScreen/Classes/_CMCommon/_view/CMBaseViewController.m
@@ -0,0 +1,26 @@
+//
+//  CMBaseViewController.m
+//  I_NScreen
+//
+//  Created by 조백근 on 2015. 9. 12..
+//  Copyright © 2015년 STVN. All rights reserved.
+//
+
+#import "CMBaseViewController.h"
+
+@interface CMBaseViewController ()
+
+@end
+
+@implementation CMBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
