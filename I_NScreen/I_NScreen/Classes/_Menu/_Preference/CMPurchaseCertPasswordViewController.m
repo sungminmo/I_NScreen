5627f1ec1fe5e620946c7bf4ac686d4be7794ed8
Benjamin <bstring.kr@gmail.com>
2015-09-30 12:38:05 +0900

    설정>구매인

--- I_NScreen/I_NScreen/Classes/_Menu/_Preference/CMPurchaseCertPasswordViewController.m
+++ I_NScreen/I_NScreen/Classes/_Menu/_Preference/CMPurchaseCertPasswordViewController.m
@@ -10,6 +10,9 @@
 
 @interface CMPurchaseCertPasswordViewController ()
 
+@property (nonatomic, strong) IBOutlet UITextField* textField1;
+@property (nonatomic, strong) IBOutlet UITextField* textField2;
+
 @end
 
 @implementation CMPurchaseCertPasswordViewController
@@ -19,6 +22,16 @@
     self.title = @"구매인증 비밀번호 관리";
     self.isUseNavigationBar = YES;
     
+    self.textField1.backgroundColor = [UIColor whiteColor];
+    self.textField1.layer.borderWidth = 1.f;
+    self.textField1.layer.cornerRadius = 4.0f;
+    self.textField1.layer.masksToBounds = YES;
+    self.textField1.layer.borderColor = [UIColor grayColor].CGColor;
+    
+    self.textField2.layer.borderWidth = 1.f;
+    self.textField2.layer.cornerRadius = 4.0f;
+    self.textField2.layer.masksToBounds = YES;
+    self.textField2.layer.borderColor = [UIColor redColor].CGColor;
 }
 
 - (void)didReceiveMemoryWarning {
