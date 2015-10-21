46c1e76473edd30ca31b2fa34e70fc2eb1c85549
jkbae(m13) <kil19c@naver.com>
2015-10-21 18:03:28 +0900

    epg 셀 통신단 작업

--- I_NScreen/I_NScreen/Classes/_Menu/_Epg/_Cell/EpgMainTableViewCell.h
+++ I_NScreen/I_NScreen/Classes/_Menu/_Epg/_Cell/EpgMainTableViewCell.h
@@ -16,6 +16,8 @@
 @property (nonatomic, weak) IBOutlet UIImageView *pLogoImageView;       // 방송로그
 @property (nonatomic, weak) IBOutlet UIImageView *pFullStreamImageView; // 총 방송 steam
 @property (nonatomic, weak) IBOutlet UIImageView *pSeeStreamImageView;  // 본 시간 steam
+@property (nonatomic, weak) IBOutlet UIImageView *pGradeImageView;      // 시청등급
+@property (nonatomic, weak) IBOutlet UIImageView *pChannelInfoImageView;    // HD 인지 SD 인지
 @property (nonatomic, weak) IBOutlet UILabel     *pChannelTitleLbl;     // 체널 타이틀
 @property (nonatomic, weak) IBOutlet UILabel     *pChannelTimeLbl;       // 체널 시간
 @property (nonatomic, weak) IBOutlet UILabel     *pChannelLbl;           // 체널 
