d4f1f6c1ee95aecbbd128c42675d4519a6ff1857
jkbae(m13) <kil19c@naver.com>
2015-10-28 14:23:57 +0900

    VOD 디테일 화면 작업 중

---
+++ I_NScreen/I_NScreen/Classes/_Menu/_Home/_VodDetail/VodDetailMainViewController.m
@@ -0,0 +1,30 @@
+//
+//  VodDetailMainViewController.m
+//  I_NScreen
+//
+//  Created by JUNG KIL BAE on 2015. 10. 28..
+//  Copyright © 2015년 STVN. All rights reserved.
+//
+
+#import "VodDetailMainViewController.h"
+
+@interface VodDetailMainViewController ()
+
+@end
+
+@implementation VodDetailMainViewController
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+
+
+
+@end
