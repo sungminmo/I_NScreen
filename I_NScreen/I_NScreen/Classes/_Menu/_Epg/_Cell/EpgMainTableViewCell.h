f86fb38b2d110e0841c8b26f5b263ffba37f7f98
benjamincho <bstring.kr@gmail.com>
2015-11-03 09:07:48 +0900

    채널 가이드 작업

--- I_NScreen/I_NScreen/Classes/_Menu/_Epg/_Cell/EpgMainTableViewCell.h
+++ I_NScreen/I_NScreen/Classes/_Menu/_Epg/_Cell/EpgMainTableViewCell.h
@@ -15,8 +15,6 @@
 @property (nonatomic, weak) IBOutlet UIImageView *pLineImageView02;     // 밑 라인
 @property (nonatomic, weak) IBOutlet UIImageView *pHeartImgView;        // 하트
 @property (nonatomic, weak) IBOutlet UIImageView *pLogoImageView;       // 방송로그
-@property (nonatomic, weak) IBOutlet UIImageView *pFullStreamImageView; // 총 방송 steam
-@property (nonatomic, weak) IBOutlet UIImageView *pSeeStreamImageView;  // 본 시간 steam
 @property (nonatomic, weak) IBOutlet UIImageView *pGradeImageView;      // 시청등급
 @property (nonatomic, weak) IBOutlet UIImageView *pChannelInfoImageView;    // HD 인지 SD 인지
 @property (nonatomic, weak) IBOutlet UILabel     *pChannelTitleLbl;     // 체널 타이틀
