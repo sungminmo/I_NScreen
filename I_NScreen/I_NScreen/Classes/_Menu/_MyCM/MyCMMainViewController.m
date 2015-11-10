bd1972bbb12846e9dca5de85094ee0f78404fae2
benjamincho <bstring.kr@gmail.com>
2015-11-10 15:27:56 +0900

    my cam 오토레이아웃 작업

--- I_NScreen/I_NScreen/Classes/_Menu/_MyCM/MyCMMainViewController.m
+++ I_NScreen/I_NScreen/Classes/_Menu/_MyCM/MyCMMainViewController.m
@@ -23,12 +23,13 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.title = @"마이 C&M";
+    self.isUseNavigationBar = YES;
     
     [self setTagInit];
     [self setViewInit];
