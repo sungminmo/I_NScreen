51997e255d6872bf4c7c51fc417533e3d28582cb
benjamincho <bstring.kr@gmail.com>
2015-11-20 14:28:21 +0900

    클래스 추가

---
+++ I_NScreen/I_NScreen/Classes/_Menu/_LeftMenu/CMLeftMenuBottomViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CMLeftMenuBottomViewCell.h
+//  I_NScreen
+//
+//  Created by 조백근 on 2015. 11. 20..
+//  Copyright © 2015년 STVN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMLeftMenuBottomViewCell : UITableViewCell
+
+@end
---
+++ I_NScreen/I_NScreen/Classes/_Menu/_LeftMenu/CMLeftMenuBottomViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CMLeftMenuBottomViewCell.m
+//  I_NScreen
+//
+//  Created by 조백근 on 2015. 11. 20..
+//  Copyright © 2015년 STVN. All rights reserved.
+//
+
+#import "CMLeftMenuBottomViewCell.h"
+
+@implementation CMLeftMenuBottomViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
